57755dab29fb39075f677790947e963c4a9ff621
EC2 Default User <benjamin.yu741@gmail.com>
2018-11-26 16:02:21 +0000

    fixed index

--- awardShow.php
+++ awardShow.php
@@ -1,3 +1,17 @@
 <?php
 	echo '<h1>Award Show Movies</h1>';
+	$dbc = new PDO('mysql:host=localhost;dbname=moviedatabase', root);
+	$movieInfo = $dbc->prepare("SELECT * FROM Movie WHERE genre = '$genre'; ");
+	$movieInfo->execute();
+	$movieInfo->bindColumn('name',$name);
+	$movieInfo->bindColumn('genre',$genre);
+	if($movieInfo->rowCount()==0) {
+		echo 'No results found';
+	}
+	while($movieInfo->fetch(PDO::FETCH_BOUND)) {
+		echo "<br>";
+		echo $name;
+		echo $genre;
+	}
+?>
 ?>
